ef04b834b7a1045719d2beb886cd5e2fe240fe2d
cdaza <cdaza933@hotmail.com>
2020-02-10 21:04:00 -0500

    add task

---
+++ backend/contest/tasks.py
@@ -0,0 +1,13 @@
+from backend.celery import app
+from video_encoding.tasks import convert_all_videos
+
+from contest.models import Video
+
+
+@app.task()
+def convert_video():
+    videos_to_convert = Video.objects.all()
+    app_label = videos_to_convert.first()._meta.app_label
+    model_name = videos_to_convert.first()._meta.model_name
+    for video in videos_to_convert:
+        convert_all_videos(app_label, model_name, video.id)
