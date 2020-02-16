80620741624208a4f97179a31dd4b08ca033b1e5
cdaza <cdaza933@hotmail.com>
2020-02-16 16:49:18 -0500

    add missing fields

--- backend/contest/models.py
+++ backend/contest/models.py
@@ -5,12 +5,13 @@ from video_encoding.fields import VideoField
 from video_encoding.models import Format
 from django.core.validators import FileExtensionValidator
 
+
 # Create your models here.
 class Contest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     image_path = models.ImageField(upload_to='images/', validators=[FileExtensionValidator(['png'])])
-    url = models.CharField(max_length=100)
+    url = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     begin_at = models.DateTimeField('date begin')
     end_at = models.DateTimeField('date end')
@@ -23,6 +24,7 @@ class Video(models.Model):
     duration = models.FloatField(editable=False, null=True)
     file = VideoField(width_field='width', height_field='height', duration_field='duration')
     format_set = GenericRelation(Format)
-    status = models.CharField(max_length=50, null=True, default='En Proceso')
-    comment = models.CharField(max_length=1000, null=True)
+    status = models.CharField(max_length=50, null=False, default='En Proceso')
+    comment = models.CharField(max_length=1000, null=False)
     email = models.EmailField(blank=False, null=False)
+    user_name = models.CharField(max_length=255, null=False)
