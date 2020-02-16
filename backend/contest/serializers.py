769f0106d4557793c457e56b75f95e3c3d3816e9
cdaza <carlos.daza@blackboard.com>
2020-02-16 16:09:53 -0500

    modify logic to send ema

--- backend/contest/serializers.py
+++ backend/contest/serializers.py
@@ -3,22 +3,23 @@ from video_encoding.models import Format
 
 from .models import Contest, Video
 
+
 class ContestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contest
-        fields='__all__'
+        fields = '__all__'
 
 
 class FormatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Format
-        fields='__all__'
+        fields = '__all__'
 
 
 class VideoSerializer(serializers.ModelSerializer):
     contest = ContestSerializer()
     format_set = FormatSerializer(many=True)
+
     class Meta:
-        model=Video
-        fields='__all__'
-        
+        model = Video
+        fields = '__all__'
