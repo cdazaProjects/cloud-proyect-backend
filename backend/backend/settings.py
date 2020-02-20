3a51ae35a55494a75ef362c7af2c4e6328d334f8
carlos <cdaza933@hotmail.com>
2020-02-19 23:41:13 -0500

    fix static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -160,9 +160,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_URL = ' https://docs.djangoproject.com/en/1.8/howto/static-files/'
+STATIC_ROOT = BASE_DIR + '/static/'
 
-STATIC_URL = '/static/'
 
 VIDEO_ENCODING_FORMATS = {
     'FFmpeg': [
