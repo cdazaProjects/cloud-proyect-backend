faba94ac00e7beca0678a00d6f535eadc40360b5
cdaza <cdaza933@hotmail.com>
2020-04-06 19:58:27 -0500

    only upload son workers

--- deployenv/bin/celery.sh
+++ deployenv/bin/celery.sh
@@ -7,10 +7,8 @@ export C_FORCE_ROOT=TRUE
 
 startCelery(){
     cd /srv/www/backend/backend
-    celery -A backend worker -P eventlet -l info -n worker1@%h -Q father --concurrency=2 --without-mingle &
     celery -A backend worker -P eventlet -l info -n worker2@%h -Q son --concurrency=2   --without-mingle &
     celery -A backend worker -P eventlet -l info -n worker3@%h -Q son --concurrency=2  --without-mingle &
-    celery -A backend worker -P eventlet -l info -n worker4@%h -Q check --concurrency=2  --without-mingle &
     celery -A backend beat
 }
 
